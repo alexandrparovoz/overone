e94d30788b4de0b16be430828f9e070d02f38eaa
User <lonzo.trans@gmail.com>
2022-11-28 20:57:07 +0300

    initial commit

---
+++ угадать_число.py
@@ -0,0 +1,14 @@
+#  примеp употребления if
+if __name__ == '__main__':
+
+    number = 23
+    guess = int(input('введите целое число: '))
+
+    if guess == number:
+        print('Perfect, you are win!')
+        print('But you are have not any prize')
+    elif guess < number:
+        print('No, guess number same more, then your number')
+    else:
+        print('No, guess number same less then your number')
+    print('Finished!')  #  Это выражение всгда выполняется после исполнения оператора if
---
+++ угадать_число_ while.py
@@ -0,0 +1,19 @@
+if __name__ =='__main__':
+
+    number = 55
+    running = True
+
+    while running:
+        guess = int(input('Bring integer: '))
+
+        if guess == number:
+            print('Grate, you are win!')
+            running = False  #  это останавливает цикл while
+        elif guess < number:
+            print('No, guess number some more then your number')
+        else:
+            print('No, guess number some less then your number')
+
+    else:
+        print('Cycle while finished')  # Я не понял смфсл применения этого предложения
+    print('Closed')
