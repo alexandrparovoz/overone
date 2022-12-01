0c0cbced7511354b5af16b657d364f46ddaa2c74
User <lonzo.trans@gmail.com>
2022-12-01 20:45:23 +0300

    initial commit

--- for.py
+++ for.py
@@ -1,5 +1,5 @@
-
-for i in range(1, 8):
-    print(i)
-else:     # Блок else выполняется только один раз после окончания цикла  for
-    print('Cycle for is finished')
+if __name__ == '__main__':
+    for i in range(1, 8):
+        print(i)
+    else:     # Блок else выполняется только один раз после окончания цикла  for
+        print('Cycle for is finished')
