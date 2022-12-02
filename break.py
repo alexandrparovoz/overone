38cbcbee7f5bb2ff8abdce5c7b29fa7188c877f1
User <lonzo.trans@gmail.com>
2022-12-02 20:08:17 +0300

    initial commit

--- break.py
+++ break.py
@@ -1,6 +1,8 @@
-while True:
-    s = input('Enter anything: ')
-    if s == 'exit':
-        break
-    print('Length string:', len(s))
-print('Finished')
+if __name__ == '__main__':
+
+    while True:
+        s = input('Enter anything: ')
+        if s == 'exit':
+             break
+        print('Length string:', len(s))
+    print('Finished')
--- function_varargs.py
+++ function_varargs.py
@@ -1,11 +1,13 @@
 # показывам переменное число параметров
 
-def total(a=5, *numbers, **phonebook):
-    print('a', a)
+if __name__ == '__main__':
 
-    for single_item in numbers:  # проходим по всем элементам КОРТЕЖА
-        print('single_item',single_item)
-    for first_part, second_part in phonebook.items():  # проходим по всем элментам  СЛВАРЯ
-        print(first_part, second_part)
+    def total(a=5, *numbers, **phonebook):
+        print('a', a)
 
-total(10, 1, 2, 3, 33, Jack=1123, John=2231, ufo=887755, Inge=1560) 
+        for single_item in numbers:  # проходим по всем элементам КОРТЕЖА
+            print('single_item',single_item)
+        for first_part, second_part in phonebook.items():  # проходим по всем элментам  СЛВАРЯ
+            print(first_part, second_part)
+
+    total(10, 1, 2, 3, 33, Jack=1123, John=2231, ufo=887755, Inge=1560)
