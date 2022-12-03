b489b2ccc9823a7ac305ec294727398d30e78b3b
User <lonzo.trans@gmail.com>
2022-12-03 18:19:06 +0300

    initial commit

--- пирожки.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-
-    cake_rub = int(input('Введите цену пирожков в рублях: '))
-    cake_pens = int(input('Введите цену ппирожков в копейках: '))
-    cake_total = int(input('Введите число пирожков:'))
-    sum_pens = (cake_total * cake_pens) % 100
-    sum_rub = (cake_total * cake_rub) + (cake_total * cake_pens) // 100
-    print('Пирожки стоят ', sum_rub, 'рублей и',sum_pens, 'копеек')
