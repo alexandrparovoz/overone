b2effafe4f84f66205413eadf8d68f94fd947012
User <lonzo.trans@gmail.com>
2022-12-03 11:22:34 +0300

    initial commit

--- function_varargs.py
+++ function_varargs.py
@@ -4,10 +4,8 @@ if __name__ == '__main__':
 
     def total(a=5, *numbers, **phonebook):
         print('a', a)
-
         for single_item in numbers:  # проходим по всем элементам КОРТЕЖА
             print('single_item',single_item)
         for first_part, second_part in phonebook.items():  # проходим по всем элментам  СЛВАРЯ
             print(first_part, second_part)
-
     total(10, 1, 2, 3, 33, Jack=1123, John=2231, ufo=887755, Inge=1560)
