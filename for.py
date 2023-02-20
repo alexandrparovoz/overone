1c911ffd4928f1b9065f7016ed6b4eee873c337b
alexandrparovoz <119344065+alexandrparovoz@users.noreply.github.com>
2023-02-20 09:53:15 +0300

    Delete for.py

--- for.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-
-    for i in range(1, 8):
-        print(i)
-    else:     # Блок else выполняется только один раз после окончания цикла  for
-        print('Cycle for is finished')
